4497cef22c18ac46b9b9e66e6065122e9daa4f97
jiajingbin <jiajaybin@126.com>
2025-05-12 20:19:26 +0800

    feat: add longbow csv datain

--- tests/cases/customer_scenarios/ems/start.py
+++ tests/cases/customer_scenarios/ems/start.py
@@ -18,8 +18,15 @@ import os
 import sys
 from datetime import datetime, timedelta
 import json
+from pathlib import Path
+import toml
 class Start(TDCase):
     def init(self):
+        # mqtt_data_source: $mqttDataSource,
+        # mqtt_processes_per_node: $mqttProcessesPerNode,
+        self.yml_name = sys.argv[1].split("=")[1]
+        self.numbers_str = ''.join(filter(str.isdigit, self.yml_name))
+        self.mqtt_node_idx = int(self.numbers_str)
         self.tdCom = TDCom(self.tdSql)
         self._remote: Remote = Remote(self.logger)
         self.env_root = os.path.join(os.environ["TEST_ROOT"], "env")
@@ -27,9 +34,33 @@ class Start(TDCase):
         self.start_time = datetime.utcnow() - timedelta(minutes=5)
         self.start_time_str = f"{self.start_time.isoformat(timespec='milliseconds')}Z"
         self.case_config["start_time"] = self.start_time_str
+        self.mqtt_data_source = self.case_config["mqtt_data_source"]
+        self.mqtt_processes_per_node = self.case_config["mqtt_processes_per_node"]
+        self.toml_file_list = list()
         with open(os.path.join(self.env_root, "workflow_config.json"), "w") as config_file:
             json.dump(self.case_config, config_file, indent=4)
         pass
+
+    def generate_idxs(self, idx, chunk_size):
+        start = (idx - 1) * chunk_size + 1
+        arr = list(range(start, start + chunk_size))
+        return arr
+
+    def generate_tomls(self, config_path):
+        with open(config_path, 'r', encoding='utf-8') as f:
+            config = toml.load(f)
+        if 'parser' not in config or 'delta' not in config['parser']:
+            raise ValueError("no parser or delta in toml file")
+
+        idx = self.generate_idxs(self.mqtt_node_idx, int(self.mqtt_processes_per_node))
+        original_path = Path(config_path)
+        for id in idx:
+            new_path = original_path.with_stem(original_path.stem + str(id))
+            self.toml_file_list.append(new_path)
+
+            config['parser']['delta'] = f'{id*2}d'
+            with open(new_path, 'w', encoding='utf-8') as f:
+                toml.dump(config, f)
 
     def start_mqtt_simulator(self):
         if "edge" in " ".join(sys.argv):
@@ -42,7 +73,25 @@ class Start(TDCase):
             #mqtt_pub_interval= mqtt_client_config["spec"]["interval"]
             mqtt_pub_interval = self.case_config["source_interval"]
             exec_time = self.case_config["exec_time"]
-            self._remote.cmd(mqtt_host,f"nohup mqtt_pub --schema {mqtt_pub_path} --host {edge_host} --interval {mqtt_pub_interval}ms --exec-duration {exec_time}s > mqtt_pub.log 2>&1 &")
+            self._remote.cmd(mqtt_host, f"nohup mqtt_pub --schema {mqtt_pub_path} --host {edge_host} --interval {mqtt_pub_interval}ms --exec-duration {exec_time}s > mqtt_pub.log 2>&1 &")
+
+    def start_longbow_datain(self):
+        if "edge" in " ".join(sys.argv):
+            mqtt_client_config = self.tdCom.get_components_setting(self.env_setting["settings"], "mqtt_client")
+            edge_config = self.tdCom.get_components_setting(self.env_setting["settings"], "taosd")
+            edge_host = edge_config["fqdn"][0]
+            mqtt_host = mqtt_client_config["fqdn"][0]
+            # mqtt_pub_path = mqtt_client_config["spec"]["config"]
+            mqtt_pub_path = mqtt_client_config["spec"]["config_file"]
+            #mqtt_pub_interval= mqtt_client_config["spec"]["interval"]
+            mqtt_pub_interval = self.case_config["source_interval"]
+            exec_time = self.case_config["exec_time"]
+            self.generate_tomls(mqtt_pub_path)
+            cmd_list = list()
+            for mqtt_toml in self.toml_file_list:
+                cmd_list.append(f"nohup mqtt_pub --csv-file /opt/longbow_recording_fuzzy.csv --csv-header topic,payload,qos,a,b,c --schema {mqtt_toml} --host {edge_host} --interval {mqtt_pub_interval}ms --exec-duration {exec_time}s > {mqtt_toml}.log 2>&1 &")
+            self._remote.cmd(mqtt_host, cmd_list)
+# ./mqtt_pub  --csv-file longbow_recording.csv --csv-header topic,payload,qos,a,b,c --schema longbow.toml --interval 0s --host ems-edge-2
 
     def start_taosx_service(self,host):
         self._remote.cmd(host,"systemctl stop taos-explorer")
@@ -55,8 +104,10 @@ class Start(TDCase):
         taosx_host = taosd_setting["fqdn"][0]
         self.start_taosx_service(taosx_host)
         # start mqtt simulator
-        self.start_mqtt_simulator()
-
+        if self.mqtt_data_source == "longbow-csv":
+            self.start_longbow_datain()
+        else:
+            self.start_mqtt_simulator()
 
     def cleanup(self):
         pass
