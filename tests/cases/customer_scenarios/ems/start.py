081db2ed3a1cf1d53da923336301a362447954a7
jiajingbin <jiajaybin@126.com>
2025-05-15 19:20:58 +0800

    enh: add step for mkdir

--- tests/cases/customer_scenarios/ems/start.py
+++ tests/cases/customer_scenarios/ems/start.py
@@ -93,6 +93,7 @@ class Start(TDCase):
             cmd_list = list()
             for mqtt_toml in self.toml_file_list:
                 self._remote.put(mqtt_host, mqtt_toml, os.path.dirname(mqtt_toml))
+                self._remote.cmd(mqtt_host, ['mkdir -p /var/log/taos'])
                 cmd_list.append(f"screen -L -Logfile /var/log/taos/{Path(mqtt_toml).stem}.log -d -m mqtt_pub --csv-file /opt/battery_storage_data.csv --csv-header topic,payload,qos,a,b,c --schema {mqtt_toml} --host {edge_host} --interval {mqtt_pub_interval}ms --exec-duration {exec_time}s")
             self._remote.cmd(mqtt_host, cmd_list)
             mqtt_start_time = time.time()
